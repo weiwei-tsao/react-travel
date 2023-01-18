1da554dd073ca55022ed9516493d836b99a5d4fc
weiwei-tsao <weiwei.tsao@hotmail.com>
2023-01-18 18:15:30 -0500

    feat: initial project

---
+++ src/custom.d.ts
@@ -0,0 +1,18 @@
+/*
+declare module "*.css" {
+    const css : {
+        [key: string]:string
+    }
+
+    export default css;
+}
+其实已经有了这个文件了：react-app-env.d.ts
+*/
+
+// declare module "*.css" {
+//     const css : {
+//         [key: string]:string
+//     }
+
+//     export default css;
+// }
