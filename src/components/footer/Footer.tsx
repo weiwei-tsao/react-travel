b6988434123f7a697de4c8f6005d87794f05de30
weiwei-tsao <weiwei.tsao@hotmail.com>
2023-01-19 09:01:49 -0500

    feat: refactor Header and Footer components

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Layout, Typography } from 'antd';
+
+interface FooterProps {}
+
+export const Footer: React.FC = (props: FooterProps) => {
+  return (
+    <div>
+      <Layout.Footer>
+        <Typography.Title level={3} style={{ textAlign: 'center' }}>
+          版权所有 @ React 旅游网
+        </Typography.Title>
+      </Layout.Footer>
+    </div>
+  );
+};
