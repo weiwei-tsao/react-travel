321b1a2113185c0d1581e39d85bf16a326fd01e2
weiwei-tsao <weiwei.tsao@hotmail.com>
2023-01-31 22:22:11 -0500

    feat: add products and business partners sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Col, Row } from 'antd';
+import { Col, Row, Typography } from 'antd';
 import styles from './App.module.css';
-import { Header, Footer, Carousel, SideMenu } from './components';
+import {
+  Header,
+  Footer,
+  Carousel,
+  SideMenu,
+  ProductCollection,
+  BusinessPartners,
+} from './components';
+import { productList1, productList2, productList3 } from './mockups';
+import sideImage from './assets/images/sider_2019_12-09.png';
+import sideImage2 from './assets/images/sider_2019_02-04.png';
+import sideImage3 from './assets/images/sider_2019_02-04-2.png';
 
 function App() {
   return (
@@ -18,6 +29,34 @@ function App() {
             <Carousel />
           </Col>
         </Row>
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="warning">
+              爆款推荐
+            </Typography.Title>
+          }
+          sideImage={sideImage}
+          products={productList1}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="danger">
+              新品上市
+            </Typography.Title>
+          }
+          sideImage={sideImage2}
+          products={productList2}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="success">
+              国内游推荐
+            </Typography.Title>
+          }
+          sideImage={sideImage3}
+          products={productList3}
+        />
+        <BusinessPartners />
       </div>
       <Footer />
     </div>
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,5 @@ export * from './header';
 export * from './footer';
 export * from './carousel';
 export * from './sideMenu';
+export * from './productCollection';
+export * from './businessPartners';
